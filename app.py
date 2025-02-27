215e86a715b952988515f101aec9792024256b70
adaniesl2 <adaniesl@eafit.edu.co>
2025-02-27 10:27:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,23 @@ st.image(image, caption = 'Flygon')
 text = st.text_input('Cuál es tu favorito?', 'Mi pokémon favorito es...')
 st.write(text+'?', 'Que buen gusto.')
 
-st.subheader(' ')
+st.subheader('Ahora usemos 2 columnas')
+
+col1 col2 = st.columns(2)
+
+with col1: 
+  st.subheader('Esta es la primera columna')
+  st.write('Las interfaces multimodales mejoran la experiencia del usuario')
+  answer = st.checkbox('Estoy de acuerdo')
+  if answer:
+    st.write('Correcto!')
+
+with col2: 
+  st.subheader('Esta es la segunda columna')
+  modo = st.radio('Qué es la modalidad principal en tu interfaz?', ('Visual', 'Auditiva', 'Táctil'))
+  if modo == 'Visual':
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'Auditiva':
+    st.write('La audición es fundamental para tu interfaz')
+  if modo == 'Táctil':
+    st.write('El tacto es fundamental para tu interfaz')
