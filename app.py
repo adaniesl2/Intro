d990149b365629e8d9355a4142517153b9334ede
adaniesl2 <adaniesl@eafit.edu.co>
2025-02-27 10:10:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Buenas")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
-st.write("Facilmente puedo realizar backend y frontend")
+st.write("Este es mi Pokémon favorito")
 image = Image.open('Flygon.jpg')
 
 st.image(image, caption = 'Flygon')
