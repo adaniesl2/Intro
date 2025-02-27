dde46f1dcf06917909b19c28c39d7d8c586160a7
adaniesl2 <adaniesl@eafit.edu.co>
2025-02-27 10:10:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Buenas")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('Flygon.jpg')
+
+st.image(image, caption = 'Flygon')
